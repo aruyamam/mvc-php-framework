d222e4c54fc1e6817f098674dd9fa05d0516f059
aruyamam <aruyamam@gmail.com>
2019-06-05 15:28:15 +0900

    Model Class

--- app/lib/helpers/functions.php
+++ app/lib/helpers/functions.php
@@ -1,8 +1,2 @@
 <?php
-
-function dnd($data) {
-   echo '<pre>';
-   var_dump($data);
-   echo '</pre>';
-   die();
-}
+require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'helpers.php');
