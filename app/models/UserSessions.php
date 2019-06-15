5afdc142b62e5cbe032abcbd73d02e6bad12e678
aruyamam <aruyamam@gmail.com>
2019-06-15 15:27:40 +0900

    Remember Me Cookie

---
+++ app/models/UserSessions.php
@@ -0,0 +1,10 @@
+<?php
+
+class UserSessions extends Model
+{
+   public function __construct()
+   {
+      $table = 'user_sessions';
+      parent::__construct($table);
+   }
+}
--- app/models/Users.php
+++ app/models/Users.php
@@ -69,6 +69,20 @@ class Users extends Model
       }
    }
 
+   public static function loginUserFromCookie()
+   {
+      $user_session_model = new UserSessions();
+      $user_session = $user_session_model->findFirst([
+         'conditions' => 'user_agent = ? AND session = ?',
+         'bind' => [Session::uagent_no_version(), Cookie::get(REMEMBER_ME_COOKIE_NAME)]
+      ]);
+      if ($user_session->user_id !== '') {
+         $user = new self((int)$user_session->user_id);
+      }
+      $user->login();
+      return $user;
+   }
+
    public function logout()
    {
       $user_agent = Session::uagent_no_version();
