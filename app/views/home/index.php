e21d4953d19535d3e9a900a7070d9bb0d57d5c8d
aruyamam <aruyamam@gmail.com>
2019-06-25 16:06:41 +0900

    Form Helpers

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,5 @@
 <?php $this->start('body'); ?>
-   <h1 class="text-center red">Welcome to Ruah MVC Framework</h1>
+<h1 class="text-center red">Welcome to Ruah MVC Framework</h1>
+<?= inputBlock('text', 'Favorite Color', 'favorite_color', 'red', ['class' => 'form-control'], ['class' => 'form-group']); ?>
+<?= submitBlock('Save', ['class' => 'btn btn-primary'], ['class' => 'text-right']) ?>
 <?php $this->end(); ?>
